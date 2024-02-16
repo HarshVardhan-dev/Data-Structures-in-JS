8160c3d09865346e400ee0e0497f31f2809f18c7
HarshVardhan Dev <harshvardhansingh.1109@gmail.com>
2024-02-16 10:34:40 +0530

    Modification to Queue Class

--- Stacks and Queues/Challenges/Ch1.js
+++ Stacks and Queues/Challenges/Ch1.js
@@ -8,19 +8,21 @@ function findBin(n) {
   var s1, s2;
   myQueue.enqueue("1");
   for (var i = 0; i < n; i++) {
-    console.log("Before", result, s1, s2);
+    console.log("Loop Body Start");
 
+    console.log(`Value to be Dequeued: ${myQueue.getFront()}`);
     result.push(myQueue.dequeue());
+
     s1 = result[i] + "0";
     s2 = result[i] + "1";
-    console.log("----------", result[i], "Queue", Queue);
+    console.log("----------", result[i]);
     myQueue.enqueue(s1);
     myQueue.enqueue(s2);
 
-    console.log("After", result, "s1-->", s1, "s2-->", s2);
+    console.log("Result", result, "s1-->", s1, "s2-->", s2);
     console.log(
-      "----------------------------------------------",
-      `Completion of ${i + 1}th iteration`
+      "***********",
+      `Completion of ${i + 1}th iteration and value of i is ${i}`
     );
   }
 
